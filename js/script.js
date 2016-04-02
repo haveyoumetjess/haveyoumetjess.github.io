7ca6f6ff8aab35e39074e0bbd8fe921b0e39e803
haveyoumetjess <jessmireau@Jesss-MacBook-Pro.local>
2016-04-02 11:25:14 -0400

    final project

---
+++ js/script.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+    function close_accordion_section() {
+        $('.accordion .accordion-section-title').removeClass('active');
+        $('.accordion .accordion-section-content').slideUp(400).removeClass('open');
+    }
+ 
+    $('.accordion-section-title').click(function(e) {
+        // Grab current anchor value
+        var currentAttrValue = $(this).attr('href');
+        // console.log(currentAttrValue);
+        
+        // Grab toggle number
+        var clientNum = currentAttrValue.replace('#accordion-', '');
+        // console.log(clientNum);
+        
+        if($(e.target).is('.active')) {
+            close_accordion_section();
+        }else {
+            close_accordion_section();
+            // Switch image
+            $('.screencap').attr('src', 'images/preview' + clientNum  + '.jpg');
+            console.log($('.screencap').attr('src'));
+            // Add active class to section title
+            $(this).addClass('active');
+            // Open up the hidden content panel
+            $('.accordion ' + currentAttrValue).slideDown(400).addClass('open'); 
+            
+            
+        }
+ 
+        e.preventDefault();
+    });
+
+
+});
