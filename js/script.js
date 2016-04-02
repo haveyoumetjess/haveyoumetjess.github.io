0ff4df62cb34fa644d50f3eddcb67a93f583026d
haveyoumetjess <jessmireau@Jesss-MacBook-Pro.local>
2016-04-02 15:20:15 -0400

    edit aim

--- js/script.js
+++ js/script.js
@@ -7,21 +7,21 @@ $(document).ready(function() {
     $('.accordion-section-title').click(function(e) {
         // Grab current anchor value
         var currentAttrValue = $(this).attr('href');
-        // console.log(currentAttrValue);
         
         // Grab toggle number
         var clientNum = currentAttrValue.replace('#accordion-', '');
-        // console.log(clientNum);
         
         if($(e.target).is('.active')) {
             close_accordion_section();
         }else {
             close_accordion_section();
+            
             // Switch image
             $('.screencap').attr('src', 'images/preview' + clientNum  + '.jpg');
-            // console.log($('.screencap').attr('src'));
+            
             // Add active class to section title
             $(this).addClass('active');
+            
             // Open up the hidden content panel
             $('.accordion ' + currentAttrValue).slideDown(400).addClass('open'); 
             
